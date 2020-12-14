b3a1924dc2b3f7f546cf05adcfa6bafb0b9be0a2
Veldas R Durai <veldasrdurai72@gmail.com>
2020-12-14 23:35:05 +0530

    started : )

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useState } from "react";
+import styled from "styled-components";
+import veldasImg from "./veldas.jpg";
 
+const Div = styled.div`
+  width:100vw;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+  &>div{width:100%;}
+  >:nth-child(1){
+    height:15vh;
+    background-color:white;
+  }
+  >:nth-child(2){
+    display:flex;
+    justify-content:center;
+    >:nth-child(1){
+      border-radius:10px;
+      width:90vw;
+      height:60vh;
+      object-fit:cover;
+      object-position:50% ${({scrolled}) => scrolled < 100 ?  (scrolled)+"%" : "100%" };
+    }
+  }
+`;
+// 
 function App() {
+  const [ scrolled , setScrolled ] = useState(0);
+  window.onscroll = () => {
+    console.log(document.documentElement.scrollTop);
+    setScrolled(document.documentElement.scrollTop);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Div scrolled={scrolled} >
+      <div>{"VELDAS R DURAI"}</div>
+      <div><img src={veldasImg} alt={"veldas"}/></div>
+      {"Hello World"}
+      <div style={{height:"1000px",backgroundColor:"brown"}}>qssqsq</div>
+    </Div>
   );
-}
+} 
 
 export default App;
